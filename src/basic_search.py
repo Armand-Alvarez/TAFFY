108fce25f4c967221d269ea9cc245de424a96e4d
Armand Alvarez <aralvarez159@gmail.com>
2025-10-07 23:17:06 -0400

    Added basic example from wikipedia for searching with their php api

--- src/basic_search.py
+++ src/basic_search.py
@@ -1,8 +1,22 @@
 import requests
+import json
+from enums import BASE_URLS, Sources
 
 
-query = input("Enter your query > ")
-url = f"https://en.wikipedia.org/wiki/{query}"
+search_query = 'solar system'
+number_of_results = 3
+endpoint = 'search/page'
+base_wiki_url = BASE_URLS[Sources.WIKIPEDIA]
 
-response = requests.get(url)
-print(response)
+# See https://www.mediawiki.org/wiki/API:REST_API#Client_identification
+headers = {'User-Agent': 'MediaWiki REST API docs examples/0.1 (https://meta.wikimedia.org/wiki/User:APaskulin_(WMF))'}
+
+url = base_wiki_url + endpoint
+response = requests.get(url, headers=headers, params={'q': search_query, 'limit': number_of_results})
+response = json.loads(response.text)
+print(f"----\n{response}\n----")
+
+#for page in response['pages']:
+#  print(page['title'])
+#  print('https://en.wikipedia.org/wiki/' + page['key'])
+#  print()
--- src/enums.py
+++ src/enums.py
@@ -7,4 +7,8 @@ class Sources(Enum):
 
 API_VERSIONS = {
     Sources.WIKIPEDIA: "1",
-    }
+}
+
+BASE_URLS = {
+    Sources.WIKIPEDIA: f"https://en.wikipedia.org/w/rest.php/{API_VERSIONS[Sources.WIKIPEDIA]}/",
+}
