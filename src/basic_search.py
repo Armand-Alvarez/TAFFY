b6534ae576818deaaf3b196680394de11b656257
Armand-Alvarez <email@armandalvarez.com>
2025-10-09 17:17:25 -0400

    Changed headers to identify the tool but provide no contact info (this is desired)

--- src/basic_search.py
+++ src/basic_search.py
@@ -9,7 +9,7 @@ endpoint = 'search/page'
 base_wiki_url = BASE_URLS[Sources.WIKIPEDIA]
 
 # See https://www.mediawiki.org/wiki/API:REST_API#Client_identification
-headers = {'User-Agent': 'MediaWiki REST API docs examples/0.1 (https://meta.wikimedia.org/wiki/User:APaskulin_(WMF))'}
+headers = {'User-Agent': 'TAFFY Searching Tool/0.1 (none)'}
 
 url = base_wiki_url + endpoint
 response = requests.get(url, headers=headers, params={'q': search_query, 'limit': number_of_results})
