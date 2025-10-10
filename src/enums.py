79153e3b57af8b011ce9bff42b6e05cd5b406882
Armand Alvarez <aralvarez159@gmail.com>
2025-10-10 00:52:16 -0400

    Added more abstraction to Enums file, also added enums for getting an actual page

--- src/enums.py
+++ src/enums.py
@@ -8,7 +8,12 @@ class API_INFORMATION(Enum):
     BASE_URL = "base_url"
     ENDPOINT = "endpoint"
     HEADERS = "headers"
+    URL = "url"
+    PARAMS = "params"
 
+class API_FUNCTIONS(Enum):
+    SEARCH_PAGES = "search_pages"
+    GET_PAGE = "get_page"
 
 API_VERSIONS = {
     Sources.WIKIPEDIA: "1",
@@ -16,10 +21,20 @@ API_VERSIONS = {
 
 CONNECTION_INFORMATION = {
     Sources.WIKIPEDIA: {
-        API_INFORMATION.BASE_URL: f"https://en.wikipedia.org/w/rest.php/v{API_VERSIONS[Sources.WIKIPEDIA]}/",
-        API_INFORMATION.ENDPOINT: 'search/page',
-        API_INFORMATION.HEADERS: {
-            'User-Agent': 'TAFFY Searching Tool/0.1 (none)'
+        API_FUNCTIONS.SEARCH_PAGES: {
+            API_INFORMATION.BASE_URL: f"https://en.wikipedia.org/w/rest.php/v{API_VERSIONS[Sources.WIKIPEDIA]}/",
+            API_INFORMATION.ENDPOINT: 'search/page',
+            API_INFORMATION.HEADERS: {
+                'User-Agent': 'TAFFY Searching Tool/0.1 (none)'
+            },
+            API_INFORMATION.PARAMS: {'q': '{query}', 'limit': '{number_of_results}'}
+        },
+        API_FUNCTIONS.GET_PAGE: {
+            API_INFORMATION.BASE_URL: f"https://en.wikipedia.org/w/rest.php/v{API_VERSIONS[Sources.WIKIPEDIA]}/",
+            API_INFORMATION.ENDPOINT: 'page/{title}/with_html',
+            API_INFORMATION.HEADERS: {
+                'User-Agent': 'TAFFY Searching Tool/0.1 (none)'
+            }
         }
     }
 }
