85b0753ade3aa6e93b8515c43bba44b1085264fe
Armand Alvarez <aralvarez159@gmail.com>
2025-10-09 20:38:53 -0400

    Changed the enums to further abstract information, adjusted the search script to respect these changes

--- src/basic_search.py
+++ src/basic_search.py
@@ -1,17 +1,14 @@
 import requests
-from enums import BASE_URLS, Sources
+from enums import Sources, CONNECTION_INFORMATION, API_INFORMATION
 import json
 
 
 search_query = 'solar system'
 number_of_results = 3
-endpoint = 'search/page'
-base_wiki_url = BASE_URLS[Sources.WIKIPEDIA]
+wiki_conn_info = CONNECTION_INFORMATION[Sources.WIKIPEDIA]
+url = wiki_conn_info[API_INFORMATION.BASE_URL] + wiki_conn_info[API_INFORMATION.ENDPOINT]
+headers = wiki_conn_info[API_INFORMATION.HEADERS]
 
-# See https://www.mediawiki.org/wiki/API:REST_API#Client_identification
-headers = {'User-Agent': 'TAFFY Searching Tool/0.1 (none)'}
-
-url = base_wiki_url + endpoint
 response = requests.get(url, headers=headers, params={'q': search_query, 'limit': number_of_results})
 response = json.loads(response.text)
 pages = response["pages"]
--- src/enums.py
+++ src/enums.py
@@ -4,11 +4,22 @@ from enum import Enum
 class Sources(Enum):
     WIKIPEDIA = "wikipedia"
 
+class API_INFORMATION(Enum):
+    BASE_URL = "base_url"
+    ENDPOINT = "endpoint"
+    HEADERS = "headers"
+
 
 API_VERSIONS = {
     Sources.WIKIPEDIA: "1",
 }
 
-BASE_URLS = {
-    Sources.WIKIPEDIA: f"https://en.wikipedia.org/w/rest.php/v{API_VERSIONS[Sources.WIKIPEDIA]}/",
+CONNECTION_INFORMATION = {
+    Sources.WIKIPEDIA: {
+        API_INFORMATION.BASE_URL: f"https://en.wikipedia.org/w/rest.php/v{API_VERSIONS[Sources.WIKIPEDIA]}/",
+        API_INFORMATION.ENDPOINT: 'search/page',
+        API_INFORMATION.HEADERS: {
+            'User-Agent': 'TAFFY Searching Tool/0.1 (none)'
+        }
+    }
 }
