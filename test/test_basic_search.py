77708229bc8ec1226977fcd13467aadd4d167461
Armand Alvarez <aralvarez159@gmail.com>
2025-10-15 19:03:53 -0400

    Removed basic_search and instead threw the files into connections directory

--- test/test_basic_search.py
+++
@@ -1,9 +0,0 @@
-from basic_search import Wikipedia as Wikipedia
-
-
-class TestWikipedia:
-    class TestSearchForPages:
-        def test_error_raised_when_non_200_status_code(self, monkeypatch):
-            w = Wikipedia()
-            
-        
