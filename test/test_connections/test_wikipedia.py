76a5bc87aa0016edbcc224f6f61795ea79942d5c
Armand Alvarez <aralvarez159@gmail.com>
2025-11-14 18:51:45 -0500

    Created new test & put tests in Class

--- test/test_connections/test_wikipedia.py
+++ test/test_connections/test_wikipedia.py
@@ -5,10 +5,19 @@ from src.connections.wikipedia import Wikipedia
 
 w = Wikipedia()
 
-@pytest.mark.parametrize("status_code", [400, 401, 402, 500, 501, 502])
-def test_raise_error_on_non_200_response(monkeypatch, mock_get, status_code):
-    mock_request_get = mock_get(status_code=status_code)
-    monkeypatch.setattr(requests, "get", mock_request_get)
+class TestWikipedia:
+    @pytest.mark.parametrize("status_code", [400, 401, 402, 500, 501, 502])
+    def test_raise_error_on_non_200_response(self, monkeypatch, mock_get, status_code):
+        mock_request_get = mock_get(status_code=status_code)
+        monkeypatch.setattr(requests, "get", mock_request_get)
 
-    with pytest.raises(ConnectionError):
-        w.search_for_pages()
+        with pytest.raises(ConnectionError):
+            w.search_for_pages()
+
+
+    def test_search_for_pages_returns_expected_response(self, monkeypatch, mock_get, sample_wiki_page_search_response):
+        mock_request_get = mock_get(status_code=200, json_data=sample_wiki_page_search_response)
+        monkeypatch.setattr(requests, "get", mock_request_get)
+
+        response = w.search_for_pages("test", number_of_results=3)
+        assert response == sample_wiki_page_search_response
