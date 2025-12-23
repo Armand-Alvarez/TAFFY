bece51d286dcabb4f593dbe31c828ba4e100de1c
Armand Alvarez <aralvarez159@gmail.com>
2025-12-23 16:58:39 -0500

    revert(enums): Removed Endpoints from enums bc it requires user input

--- src/connections/enums.py
+++ src/connections/enums.py
@@ -3,11 +3,3 @@ from enum import Enum
 
 class BaseUrls(Enum):
     WIKIPEDIA = "https://en.wikipedia.org/w/rest.php/v1"
-
-
-class WikipediaEndpoints(Enum):
-    SEARCH_PAGES = f"{BaseUrls.WIKIPEDIA.value}/search/page"
-    GET_PAGE = f"{BaseUrls.WIKIPEDIA}//page/{0}/bare"
-    GET_PAGE_WITH_HTML = (
-        lambda user_value: f"{BaseUrls.WIKIPEDIA}/page/{user_value}/with_html"
-    )
