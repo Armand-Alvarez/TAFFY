5f3e3cc4da189b0b4c34b8fb64589ec6d2c1f154
Armand Alvarez <aralvarez159@gmail.com>
2025-12-25 14:17:56 -0500

    test(connections): Added unit tests

--- tests/connections/test_wikipedia.py
+++ tests/connections/test_wikipedia.py
@@ -1,6 +1,6 @@
 import pytest
 
-from connections.wikipedia import search_pages, get_page
+from connections.wikipedia import get_page, search_pages, get_page_with_html
 
 
 class TestSearchPages:
@@ -52,45 +52,92 @@ class TestSearchPages:
 class TestGetPage:
     def test_200_status_successfully_returns_response(
         self,
-        mock_search_endpoint,
+        mock_get_page_endpoint,
         mock_get_200_status,
     ):
-        result = search_pages("test")
+        result = get_page("test")
 
         assert result == {"query": "test", "results": []}
-        mock_search_endpoint.assert_called_once_with("test")
-        mock_get_200_status.assert_called_once_with("http://example.com/search")
+        mock_get_page_endpoint.assert_called_once_with("test")
+        mock_get_200_status.assert_called_once_with("http://example.com/get_page")
 
     def test_non_200_status_code_raises_connection_error_with_string(
         self,
-        mock_search_endpoint,
+        mock_get_page_endpoint,
         mock_get_404_status,
     ):
         expected_error_string = (
-            "search_pages endpoint did not return 200. Code: 404\nerror: Not found"
+            "get_page endpoint did not return 200. Code: 404\nerror: Not found"
         )
 
         with pytest.raises(
             ConnectionError,
             match=expected_error_string,
         ):
-            search_pages("test")
+            get_page("test")
 
     def test_request_exception_raises_connection_error_with_string(
         self,
-        mock_search_endpoint,
+        mock_get_page_endpoint,
         mock_get_with_exception_side_effect,
     ):
         expected_error_string = "Failed to connect to Wikipedia API: bad request"
         with pytest.raises(ConnectionError, match=expected_error_string):
-            search_pages("test")
+            get_page("test")
 
     def test_query_gets_converted_to_string(
         self,
-        mock_search_endpoint,
+        mock_get_page_endpoint,
         mock_get_200_status,
     ):
-        search_pages(123)
+        get_page(123)
 
-        mock_search_endpoint.assert_called_once_with("123")
+        mock_get_page_endpoint.assert_called_once_with("123")
 
+
+class TestGetPageWithHTML:
+    def test_200_status_successfully_returns_response(
+        self,
+        mock_get_page_with_html_endpoint,
+        mock_get_200_status,
+    ):
+        result = get_page_with_html("test")
+
+        assert result == {"query": "test", "results": []}
+        mock_get_page_with_html_endpoint.assert_called_once_with("test")
+        mock_get_200_status.assert_called_once_with(
+            "http://example.com/get_page_with_html"
+        )
+
+    def test_non_200_status_code_raises_connection_error_with_string(
+        self,
+        mock_get_page_with_html_endpoint,
+        mock_get_404_status,
+    ):
+        expected_error_string = (
+            "get_page_with_html endpoint did not return 200. Code: 404\nerror: Not found"
+        )
+
+        with pytest.raises(
+            ConnectionError,
+            match=expected_error_string,
+        ):
+            get_page_with_html("test")
+
+    def test_request_exception_raises_connection_error_with_string(
+        self,
+        mock_get_page_with_html_endpoint,
+        mock_get_with_exception_side_effect,
+    ):
+        expected_error_string = "Failed to connect to Wikipedia API: bad request"
+        with pytest.raises(ConnectionError, match=expected_error_string):
+            get_page_with_html("test")
+
+    def test_query_gets_converted_to_string(
+        self,
+        mock_get_page_with_html_endpoint,
+        mock_get_200_status,
+    ):
+        get_page_with_html(123)
+
+        mock_get_page_with_html_endpoint.assert_called_once_with("123")
