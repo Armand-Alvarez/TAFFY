6c3297a2b4458fde15f508aec9514d56585be4b2
Charlotte <email@armandalvarez.com>
2026-01-03 02:01:25 -0500

    feat(enums): Added headers and limits to enums

--- src/connections/enums.py
+++ src/connections/enums.py
@@ -3,3 +3,13 @@ from enum import Enum
 
 class BaseUrls(Enum):
     WIKIPEDIA = "https://en.wikipedia.org/w/rest.php/v1"
+
+
+class Headers(Enum):
+    WIKIPEDIA = {
+        "User-Agent": "TAFFY: Terminal Answer Finder For You! Project at (github.com/Armand-Alvarez/TAFFY)"
+    }
+
+
+class Limits(Enum):
+    WIKIPEDIA = 3
